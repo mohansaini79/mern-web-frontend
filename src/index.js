f69fd83ad07b59c3738f4670c29984dc6383fbec
mohansaini79 <mohansaini798330@gmail.com>
2025-07-22 23:26:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './App.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
-    <App />
-  </GoogleOAuthProvider>
-);
